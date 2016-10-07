2610d7823fa918edb5333b87ec8fa53cb56c2031
user <user@FG11>
2016-10-07 09:57:11 +0800

    HW2

---
+++ src/fcu/iecs/oop/StringComparator.java
@@ -0,0 +1,24 @@
+package fcu.iecs.oop;
+import java.util.Scanner;
+public class StringComparator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println("Enter a string1:");
+		String str1=keyboard.next();
+		System.out.println("Enter a string2:");
+		String str2=keyboard.next();
+		int check=str1.compareTo(str2);
+		if(check==0){
+			
+			System.out.println("The two string are the same");
+			
+		}
+		else{
+			System.out.println("The two string are not the same");
+		}
+	}
+
+
+
+}
