070a54fef4708f75cfecbccacd2d92f5f19ee248
user <user@FG11>
2016-10-07 09:41:49 +0800

    HW2

---
+++ src/fcu/iecs/oop/CheckOddEven.java
@@ -0,0 +1,22 @@
+package fcu.iecs.oop;
+import java.util.Scanner;
+public class CheckOddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println("Enter a integer:");
+		int integer=keyboard.nextInt();
+		int check=integer%2;
+		
+		if(check==1){
+			
+			System.out.printf("%d is Odd Number",integer);
+			
+		}
+		else{
+			System.out.printf("%d is  Even Number",integer);
+		}
+	}
+
+}
